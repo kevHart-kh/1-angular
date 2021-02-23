0da2d11246adb3209963f8a8fc6d9eed104d1524
kevHart-kh <c14180077@john.petra.ac.id>
2021-02-23 19:31:45 +0700

    DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  board: Array<Array<String>> = Array<Array<String>>();
+
+  giliran = 0;
+
+  baris: number = 0;
+  kolom: number = 0;
+
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.baris = 0;
+    this.kolom = 0;
+    this.giliran = 0;
+
+    // alert("this.board1");
+    for (var i = 0; i < 5; i++) {
+      var temp = ["*", "*", "*", "*", "*"];
+      this.board[i] = temp;
+    }
+  }
+
+  submit() {
+    var tempBoard = this.board;
+
+    if (tempBoard[this.baris][this.kolom] != "*") {
+      alert("koordinat yang dipilih telah dipilih");
+      return;
+    }
+    tempBoard[this.baris][this.kolom] = "" + this.giliran;
+
+    var isMenang = false;
+    //horizontal
+    for (var i = 0; i < 5 && !isMenang; i++) {
+      var count = 0;
+      for (var j = 0; j < 5 && !isMenang; j++) {
+        var isFound = false;
+        if (tempBoard[i][j] == "" + this.giliran) {
+          count++;
+          isFound = true;
+        } else if (isFound) {
+          break;
+        }
+      }
+      if (count == 4) {
+        alert("USER " + this.giliran + " MENANGG");
+        this.reset();
+      }
+    }
+
+    //vertikal
+    for (var i = 0; i < 5 && !isMenang; i++) {
+      var count = 0;
+      for (var j = 0; j < 5 && !isMenang; j++) {
+        var isFound = false;
+        if (tempBoard[j][i] == "" + this.giliran) {
+          count++;
+          isFound = true;
+        } else if (isFound) {
+          break;
+        }
+      }
+      if (count == 4) {
+        alert("USER " + this.giliran + " MENANGG");
+        this.reset();
+      }
+    }
+
+    this.giliran = (this.giliran + 1) % 2;
+  }
 }
